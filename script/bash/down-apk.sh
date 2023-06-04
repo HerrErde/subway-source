499d807e7567f5559d8f05d1d75ee47712f7e7ff
Herr Erde <Natodamm@hotmail.com>
2023-06-04 19:35:55 +0200

    update apk download

--- script/bash/down-apk.sh
+++ script/bash/down-apk.sh
@@ -8,7 +8,6 @@ fi
 userAgent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
 orgName="sybo-games"
 appName="subwaysurfers"
-appName2="subway-surfers"
 
 appVer="$1"
 
@@ -16,14 +15,11 @@ page1=$(curl -vsL -A "$userAgent" "https://www.apkmirror.com/apk/$orgName/$appNa
 
 grep -q 'class="error404"' <<<"$page1" && echo noversion >&2 && exit 1
 
-#page2=$(pup -p --charset utf-8 ':parent-of(:parent-of(span:contains("APK")))' <<<"$page1")
+page2=$(pup -p --charset utf-8 ':parent-of(:parent-of(span:contains("APK")))' <<<"$page1")
 
-#readarray -t url1 < <(pup -p --charset utf-8 ':parent-of(div:contains("arm64-v8a + armeabi-v7a")) attr{href}' <<<"$page2")
+readarray -t url1 < <(pup -p --charset utf-8 ':parent-of(div:contains("arm64-v8a + armeabi-v7a")) attr{href}' <<<"$page2")
 
-#[ "${#url1[@]}" -eq 0 ] && echo noapk >&2 && exit 1
-
-#temp fix
-url1="/apk/$orgName/$appName/$appName-$appVer-release/$appName2-$appVer-android-apk-download/"
+[ "${#url1[@]}" -eq 0 ] && echo noapk >&2 && exit 1
 
 url2=$(curl -sL -A "$userAgent" "https://www.apkmirror.com$url1" | pup -p --charset utf-8 'a:contains("Download APK") attr{href}')
 
