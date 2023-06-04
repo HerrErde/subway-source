cebf27c9cb6a2b752bbe2574593c99826f09e9de
Herr Erde <Natodamm@hotmail.com>
2023-06-04 21:36:49 +0200

    fix sort data items
    _sort files are now right sorted and added a track number

--- misc/sort_boards.py
+++ misc/sort_boards.py
@@ -1,19 +1,20 @@
 import json
 
 
-json_input = 'boards_output.json'
-json_input_links = 'boards_links.json'
-json_output = 'boards_sort.json'
+json_input = "boards_output.json"
+json_input_links = "boards_links.json"
+json_output = "boards_data.json"
+
 
 def extract(json_input_links):
-    with open(json_input_links, 'r') as f:
+    with open(json_input_links, "r") as f:
         data = json.load(f)
 
-    names = [item.get('name', '') for item in data]
+    names = [item.get("name", "") for item in data]
     names = [name.replace(" ", "") for name in names]
     names = [name.replace("Hoverboard", "default") for name in names]
     names = [name.replace("8thBirthday", "birthday") for name in names]
-    
+
     year = 2021
     for i in range(2, len(names) + 1):
         exponent = i + 7
@@ -23,16 +24,30 @@ def extract(json_input_links):
 
     return names
 
+
 def sort_json(json_input, names, json_output):
-    with open(json_input, 'r') as f:
+    with open(json_input, "r") as f:
         data = json.load(f)
 
     names = [name.lower() for name in names]
 
-    ordered_data = [item for name in names for item in data if 'id' in item and item['id'].replace(" ", "").lower() == name]
+    ordered_data = []
+    count = 1
 
-    with open(json_output, 'w') as f:
+    for name in names:
+        for item in data:
+            if "id" in item and item["id"].replace(" ", "").lower() == name:
+                item_with_number = {
+                    "number": count,
+                    "id": item["id"],
+                    "upgrades": item["upgrades"],
+                }
+                count += 1
+                ordered_data.append(item_with_number)
+
+    with open(json_output, "w") as f:
         json.dump(ordered_data, f, indent=2)
+
 
 names = extract(json_input_links)
 sort_json(json_input, names, json_output)
--- misc/sort_characters.py
+++ misc/sort_characters.py
@@ -1,29 +1,43 @@
 import json
 
+json_input = "characters_output.json"
+json_input_links = "characters_links.json"
+json_output = "characters_data.json"
 
-json_input = 'characters_output.json'
-json_input_links = 'characters_links.json'
-json_output = 'characters_sort.json'
 
 def extract(json_input_links):
-    with open(json_input_links, 'r') as f:
+    with open(json_input_links, "r") as f:
         data = json.load(f)
 
-    names = [item.get('name', '') for item in data]
+    names = [item.get("name", "") for item in data]
     names = [name.replace("Hoverboard", "default").replace(" ", "") for name in names]
 
     return names
 
+
 def sort_json(json_input, names, json_output):
-    with open(json_input, 'r') as f:
+    with open(json_input, "r") as f:
         data = json.load(f)
 
     names = [name.lower() for name in names]
 
-    ordered_data = [item for name in names for item in data if 'id' in item and item['id'].replace(" ", "").lower() == name]
+    ordered_data = []
+    count = 1
 
-    with open(json_output, 'w') as f:
+    for name in names:
+        for item in data:
+            if "id" in item and item["id"].replace(" ", "").lower() == name:
+                item_with_number = {
+                    "number": count,
+                    "id": item["id"],
+                    "outfits": item["outfits"],
+                }
+                count += 1
+                ordered_data.append(item_with_number)
+
+    with open(json_output, "w") as f:
         json.dump(ordered_data, f, indent=2)
+
 
 names = extract(json_input_links)
 sort_json(json_input, names, json_output)
