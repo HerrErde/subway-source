d0a15afad2579e0dd53508fdc18939c6e1eb479a
Herr Erde <Natodamm@hotmail.com>
2023-06-12 00:14:01 +0200

    fix missing boards
    fix missing boards which id are not same as in wiki

--- misc/sort_boards.py
+++ misc/sort_boards.py
@@ -1,21 +1,21 @@
 import json
 import re
 
-
 json_input = "boards_output.json"
 json_input_links = "boards_links.json"
 json_output = "boards_data.json"
 
+ignore_strings = ["nflpa", "sakar"]
+
 
 def extract(json_input_links):
     with open(json_input_links, "r") as f:
-        data = json.load(f)
+        link_data = json.load(f)
 
-    names = [item.get("name", "") for item in data]
-    names = [name.replace(" ", "").replace("-", "") for name in names]
+    names = [item.get("name", "") for item in link_data]
+    names = [name.replace(" ", "").replace("-", "").replace("&", "").replace("Jr.", "")for name in names]
     names = [name.replace("Wrapped", "xmas2022") for name in names]
-    names = [re.sub(r"\bHoverboard\b", "default", name) for name in names]
-    names = [name.replace("8thBirthday", "birthday") for name in names]
+    names = [re.sub(r"\bHoverboard\b", "default", name).replace("8thBirthday", "birthday")for name in names]
 
     year = 2021
     for i in range(2, len(names) + 1):
@@ -24,6 +24,8 @@ def extract(json_input_links):
         names = [name.replace(birthday, f"birthday{year}") for name in names]
         year += 1
 
+    names = [name.lower() for name in names]
+
     return names
 
 
@@ -31,28 +33,27 @@ def sort_json(json_input, names, json_output):
     with open(json_input, "r") as f:
         data = json.load(f)
 
-    names = [name.lower() for name in names]
-
     ordered_data = []
     count = 1
     skipped_names = []
 
-    for name in names:
-        found = False
-        for item in data:
-            if "id" in item and item["id"].replace(" ", "").lower() == name:
+    for item in data:
+        if "id" in item:
+            item_id = item["id"].lower()
+            for ignore in ignore_strings:
+                item_id = item_id.split(ignore)[-1].lower()
+
+            # Check if the modified item_id is present in the names list
+            if item_id in names:
                 item_with_number = {
                     "number": count,
                     "id": item["id"],
-                    "upgrades": item["upgrades"],
+                    "upgrades": item.get("upgrades", ""),
                 }
                 count += 1
                 ordered_data.append(item_with_number)
-                found = True
-                break
-
-        if not found:
-            skipped_names.append(name)
+            else:
+                skipped_names.append(item["id"])
 
     with open(json_output, "w") as f:
         json.dump(ordered_data, f, indent=2)
@@ -62,6 +63,9 @@ def sort_json(json_input, names, json_output):
 
 names = extract(json_input_links)
 skipped_names = sort_json(json_input, names, json_output)
-print("Skipped Names:")
-for name in skipped_names:
-    print(name)
+
+if skipped_names:
+    print("Skipped Names:")
+    for name in skipped_names:
+        print(name)
+
