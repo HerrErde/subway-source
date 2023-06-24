e3b2c00546a504b9dbd4e4fe9b24a4e6a7fe7f97
Herr Erde <Natodamm@hotmail.com>
2023-06-24 19:45:03 +0200

    final fix

--- misc/sort_boards.py
+++ misc/sort_boards.py
@@ -1,5 +1,6 @@
 import json
 import re
+import sys
 
 json_input = "boards_output.json"
 json_input_links = "boards_links.json"
@@ -12,24 +13,34 @@ def extract(json_input_links):
     with open(json_input_links, "r") as f:
         link_data = json.load(f)
 
-    names = [item.get("name", "") for item in link_data]
-    names = [name.replace(" ", "").replace("-", "").replace("&", "").replace("Jr.", "")for name in names]
-    names = [name.replace("Wrapped", "xmas2022") for name in names]
-    names = [re.sub(r"\bHoverboard\b", "default", name).replace("8thBirthday", "birthday")for name in names]
+    link_names = [
+        re.sub(
+            r"\bhoverboard\b",
+            "default",
+            item.get("name", "")
+            .replace("Wrapped", "xmas2022")
+            .replace("8th Birthday", "birthday")
+            .replace("Color Shift", "colourshift")
+            .replace(" ", "")
+            .replace("-", "")
+            .replace("&", "")
+            .replace("Jr.", "")
+            .lower(),
+        )
+        for item in link_data
+    ]
 
     year = 2021
-    for i in range(2, len(names) + 1):
+    for i in range(2, len(link_names) + 1):
         exponent = i + 7
-        birthday = f"{exponent}thBirthday"
-        names = [name.replace(birthday, f"birthday{year}") for name in names]
+        birthday = f"{exponent}thbirthday"
+        link_names = [name.replace(birthday, f"birthday{year}") for name in link_names]
         year += 1
 
-    names = [name.lower() for name in names]
-
-    return names
+    return link_names
 
 
-def sort_json(json_input, names, json_output):
+def sort_json(json_input, link_names, json_output):
     with open(json_input, "r") as f:
         data = json.load(f)
 
@@ -37,23 +48,26 @@ def sort_json(json_input, names, json_output):
     count = 1
     skipped_names = []
 
-    for item in data:
-        if "id" in item:
-            item_id = item["id"].lower()
-            for ignore in ignore_strings:
-                item_id = item_id.split(ignore)[-1].lower()
+    for name in link_names:
+        found = False
+        for item in data:
+            if "id" in item:
+                item_id = item["id"].lower()
+                for ignore in ignore_strings:
+                    item_id = item_id.replace(ignore, "").lower().replace(" ", "")
+                if item_id == name:
+                    item_with_number = {
+                        "number": count,
+                        "id": item["id"],
+                        "upgrades": item.get("upgrades", ""),
+                    }
+                    count += 1
+                    ordered_data.append(item_with_number)
+                    found = True
+                    break
 
-            # Check if the modified item_id is present in the names list
-            if item_id in names:
-                item_with_number = {
-                    "number": count,
-                    "id": item["id"],
-                    "upgrades": item.get("upgrades", ""),
-                }
-                count += 1
-                ordered_data.append(item_with_number)
-            else:
-                skipped_names.append(item["id"])
+        if not found:
+            skipped_names.append(name)
 
     with open(json_output, "w") as f:
         json.dump(ordered_data, f, indent=2)
@@ -61,11 +75,11 @@ def sort_json(json_input, names, json_output):
     return skipped_names
 
 
-names = extract(json_input_links)
-skipped_names = sort_json(json_input, names, json_output)
+link_names = extract(json_input_links)
+skipped_names = sort_json(json_input, link_names, json_output)
 
 if skipped_names:
     print("Skipped Names:")
     for name in skipped_names:
         print(name)
-
+    sys.exit("Error: Items in skipped list.")
--- misc/sort_characters.py
+++ misc/sort_characters.py
@@ -4,32 +4,48 @@ json_input = "characters_output.json"
 json_input_links = "characters_links.json"
 json_output = "characters_data.json"
 
+ignore_strings = ["nflpa"]
+
+other_strings = ["dak", "lamar", "tom", "odell", "patrick", "saquon"]
+
 
 def extract(json_input_links):
     with open(json_input_links, "r") as f:
         link_data = json.load(f)
 
-    names = [item.get("name", "") for item in link_data]
-    names = [
-        name.replace("Super Runner Fernando", "fernando").replace(" ", "")
-        for name in names
+    link_names = [item.get("name", "") for item in link_data]
+    link_names = [
+        name
+        .replace("Super Runner Fernando", "fernando")
+        .replace(" ", "")
+        .replace(".", "")
+        .lower()
+        for name in link_names
     ]
 
-    return names
+    return link_names
 
 
-def sort_json(json_input, names, json_output):
+def sort_json(json_input, link_names, json_output):
     with open(json_input, "r") as f:
         data = json.load(f)
 
-    names = [name.lower() for name in names]
-
     ordered_data = []
+    skipped_names = []
     count = 1
 
-    for name in names:
-        for item in data:
-            if "id" in item and item["id"].lower() == name:
+    for item in data:
+        if "id" in item:
+            item_id = item["id"].lower()
+            for ignore in ignore_strings:
+                item_id = item_id.replace(ignore, "")
+
+            for other in other_strings:
+                if other in item_id:
+                    item_id = item_id.split(other)[0] + other
+
+            # Check if the modified item_id is present in the names list
+            if item_id in link_names:
                 item_with_number = {
                     "number": count,
                     "id": item["id"],
@@ -37,10 +53,20 @@ def sort_json(json_input, names, json_output):
                 }
                 count += 1
                 ordered_data.append(item_with_number)
+            else:
+                skipped_names.append(item["id"])
 
     with open(json_output, "w") as f:
         json.dump(ordered_data, f, indent=2)
 
+    return skipped_names
 
-names = extract(json_input_links)
-sort_json(json_input, names, json_output)
+
+link_names = extract(json_input_links)
+skipped_names = sort_json(json_input, link_names, json_output)
+
+if skipped_names:
+    print("Skipped:")
+    for name in skipped_names:
+        print(name)
+    sys.exit("Error: Items in skipped list.")
