e250a5f6c78d21b9b85783c2fbd8e19ff2fc7b0d
Herr Erde <Natodamm@hotmail.com>
2023-08-07 15:17:28 +0200

    Update fetch_outfits.py

--- misc/fetch_outfits.py
+++ misc/fetch_outfits.py
@@ -1,7 +1,7 @@
 import json
+import sys
 import requests
 from bs4 import BeautifulSoup
-import sys
 
 
 def fetch_outfits(session, entry):
@@ -9,17 +9,25 @@ def fetch_outfits(session, entry):
     url = f"https://subwaysurf.fandom.com/wiki/{name}"
     print(f"'{entry['name']}': {url}")
 
-    response = session.get(url)
-    content = response.content
-    soup = BeautifulSoup(content, "html.parser")
+    try:
+        response = session.get(url)
+        response.raise_for_status()
+        content = response.content
+        soup = BeautifulSoup(content, "html.parser")
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+        return {"name": entry["name"], "outfits": None}
+    except Exception as err:
+        print(f"Error occurred: {err}")
+        return {"name": entry["name"], "outfits": None}
+
     toc = soup.find(id="toc")
 
     if toc is None:
         print("Error: toc element not found.")
         return {"name": entry["name"], "outfits": None}
 
-    toctext_spans = toc.find_all("span", class_="toctext")
-    exclude = [
+    exclude = {
         "Appearance",
         "History",
         "Trivia",
@@ -32,26 +40,39 @@ def fetch_outfits(session, entry):
         "Re-sale",
         "Re-sales",
         "References",
-    ]
+    }
+
+    toctext_spans = toc.find_all("span", class_="toctext")
     outfits = [
-        {"name": span.get_text(strip=True), "url": ""}
+        {
+            "name": span.get_text(strip=True),
+            "url": "",
+        }
         for span in toctext_spans
         if span.get_text(strip=True) not in exclude
     ]
+
     tabber_div = soup.find("div", class_="tabber wds-tabber")
 
     if tabber_div is None:
         print("Error: tabber div not found.")
         return {"name": entry["name"], "outfits": None}
 
-    for i, tab_content_div in enumerate(
-        tabber_div.find_all("div", class_="wds-tab__content")
-    ):
+    tab_content_divs = tabber_div.find_all("div", class_="wds-tab__content")
+
+    if len(outfits) != len(tab_content_divs):
+        print(
+            f"Error: Number of outfits ({len(outfits)}) doesn't match the number of tab content divs ({len(tab_content_divs)})."
+        )
+        return {"name": entry["name"], "outfits": None}
+
+    for i, tab_content_div in enumerate(tab_content_divs):
         outfit_name = tab_content_div.find("a").get_text(strip=True)
         img_tag = tab_content_div.find("img")
-        img_url = img_tag["src"]
-        img_url = img_url.split(".png")[0] + ".png"
-        outfits[i]["url"] = img_url
+        if img_tag:
+            img_url = img_tag["src"]
+            img_url = img_url.replace(".png", "_hd.png")
+            outfits[i]["url"] = img_url
 
     return {"name": entry["name"], "outfits": outfits}
 
