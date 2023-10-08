89c7bb7d863729fffb3276e9dfb704dcafeaa4e3
Herr Erde <Natodamm@hotmail.com>
2023-10-08 13:35:10 +0200

    some change
    more checks
    improved all.py

--- all.py
+++ all.py
@@ -1,26 +1,40 @@
 import subprocess
 import time
 import requests
+import os
+import shutil
 
 
 def version():
     # get version from gplayapi
-    response = requests.get("https://gplayapi.herrerde.xyz/api/apps/com.kiloo.subwaysurf")
+    response = requests.get(
+        "https://gplayapi.cashlessconsumer.in/api/apps/com.kiloo.subwaysurf"
+    )
     data = response.json()
     version = data["version"].replace(".", "-")
 
     return version
 
 
+# Check if the "gamedata" directory exists and delete it if it does
+if os.path.exists("gamedata"):
+    shutil.rmtree("gamedata")
+
+# Create the "upload" directory if it doesn't exist
+if not os.path.exists("upload"):
+    os.mkdir("upload")
+
 scripts = [
-    #["misc/fetch_links.py"],
-    ["script/down-apk.py", version()],
-    ["misc/unpack.py", version(), "subwaysurfers"],
-    ["script/fetch_characters.py"],
-    ["script/fetch_boards.py"],
+    #["script/fetch_links.py"],
+    #["script/fetch_outfits.py"],
+    #["script/down-apk.py", version()],
+    #["misc/unpack.py", version(), "subwaysurfers"],
+    #["script/fetch_characters.py"],
+    #["script/fetch_boards.py"],
+    #["script/collections.py"],
     ["misc/sort_characters.py"],
     ["misc/sort_boards.py"],
-    #["misc/check.py"],
+    ["misc/check.py"],
     # Add more script commands and arguments as needed
 ]
 
