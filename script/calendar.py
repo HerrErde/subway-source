bd5becd5ff668f49dff3fba1080808245fa3691a
Herr Erde <Natodamm@hotmail.com>
2023-10-14 23:50:20 +0200

    add calendar

---
+++ script/calendar.py
@@ -0,0 +1,14 @@
+import json
+
+input_file_path = "gamedata/calendars.json"
+output_file_path = "upload/calendar_data.json"
+
+with open(input_file_path, "r") as input_file:
+    data = json.load(input_file)
+    calendars = data.get("calendars", [])
+    calendar_id = calendars[0].get("id")
+
+output_data = {"calendarId": calendar_id}
+
+with open(output_file_path, "w") as output_file:
+    json.dump(output_data, output_file, indent=2)
